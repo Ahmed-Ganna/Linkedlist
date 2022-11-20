8e67828d5da73a640173e82c5f6737b0d8c8db59
Ahmed Ganna <dev.ahmedg@gmail.com>
2022-11-20 15:25:06 +0100

    Delete first node of a Singly Linked List

---
+++ src/main/kotlin/SingleLinkedList.kt
@@ -0,0 +1,73 @@
+class SingleLinkedList{
+
+    var head : ListNode ? = null
+
+    class ListNode(val data : Int , var next : ListNode? = null)
+
+    fun display() {
+        var current  = head
+        while ( current!=null ){
+            print("${current.data} -->")
+            current = current.next
+        }
+        print("null")
+    }
+
+    fun length(): Int {
+        if (head ==null) return 0
+
+        var count = 0
+        var current  = head
+        while ( current!=null ){
+            count++
+            current = current.next
+        }
+        return count
+    }
+
+    fun insertFirst(data: Int) {
+        val item = ListNode(data,head)
+        head = item
+    }
+
+    fun insertLast(data: Int) {
+        val item = ListNode(data)
+        if (head ==null){
+            head = item
+            return
+        }
+        var current = head
+        while (current?.next!=null){
+            current = current.next
+        }
+        current?.next = item
+    }
+
+    fun insert(position: Int, value: Int) {
+
+        val node = ListNode(value)
+        if (position ==1 ){
+            node.next = head
+            head = node
+        }else{
+            //1 --> 2 --> 3
+            //1 --> 4 --> 2 --> 3
+            var previous = head
+            var count = 1
+            while (count < position-1){
+                previous = previous?.next
+                count++
+            }
+            node.next = previous?.next
+            previous?.next = node
+        }
+    }
+
+    fun deleteFirst() :ListNode? {
+        if (head ==null) return null
+        val tmp = head
+        head = tmp?.next
+        return tmp
+    }
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,74 +1,9 @@
 
-class SingleLinkedList{
 
-    var head : ListNode ? = null
-
-    class ListNode(val data : Int , var next : ListNode? = null)
-
-    fun display() {
-        var current  = head
-        while ( current!=null ){
-            print("${current.data} -->")
-            current = current.next
-        }
-        print("null")
-    }
-
-    fun length(): Int {
-        if (head ==null) return 0
-
-        var count = 0
-        var current  = head
-        while ( current!=null ){
-            count++
-            current = current.next
-        }
-        return count
-    }
-
-    fun insertFirst(data: Int) {
-        val item = ListNode(data,head)
-        head = item
-    }
-
-    fun insertLast(data: Int) {
-        val item = ListNode(data)
-        if (head ==null){
-            head = item
-            return
-        }
-        var current = head
-        while (current?.next!=null){
-            current = current.next
-        }
-        current?.next = item
-    }
-
-    fun insert(position: Int, value: Int) {
-
-        val node = ListNode(value)
-        if (position ==1 ){
-            node.next = head
-            head = node
-        }else{
-            //1 --> 2 --> 3
-            //1 --> 4 --> 2 --> 3
-            var previous = head
-            var count = 1
-            while (count < position-1){
-                previous = previous?.next
-                count++
-            }
-            node.next = previous?.next
-            previous?.next = node
-        }
-    }
-
-}
 
 fun main(args: Array<String>) {
 
-    val sll = SingleLinkedList()
+/*    val sll = SingleLinkedList()
     sll.head = SingleLinkedList.ListNode(3)
     val second = SingleLinkedList.ListNode(4)
     val third = SingleLinkedList.ListNode(5)
@@ -86,9 +21,10 @@ fun main(args: Array<String>) {
 
     sll.display()
 
-    println("LLS length -> ${sll.length()}")
+    println("LLS length -> ${sll.length()}")*/
 
 
+    deleteFirst()
 }
 
 fun insertAtIndex() {
@@ -98,5 +34,14 @@ fun insertAtIndex() {
     sll.insert(1 , 3) // 3 --> 1 --> 2
     sll.insert(2 , 4) // 3 --> 4 --> 1 --> 2
 
+    sll.display()
+}
+
+fun deleteFirst() {
+    val sll = SingleLinkedList()
+    sll.insertFirst(2)
+    sll.insertFirst(1)
+    sll.display()
+    sll.deleteFirst()
     sll.display()
 }
